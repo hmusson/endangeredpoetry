9f33dfccd842f7ed8c28458cdfec155ee0d608e0
Benji Friedman <benjifriedman@Benji.local>
2017-03-31 12:38:39 -0700

    march 31 changes

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('./app_api/models/db');
+
+var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
 
 var app = express();
 
-var index = require('./app_server/routes/index');
-app.use('/', index);
+// var index = require('./app_server/routes/index');
+// app.use('/', index);
 
-var users = require('./app_server/routes/users');
-app.use('/users', users);
+// var users = require('./app_server/routes/users');
+// app.use('/users', users);
 
-var poemList = require('./app_server/routes/poemList');
-app.use('/', poemList);
-
+// var poemList = require('./app_server/routes/poemList');
+// app.use('/', poemList);
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
@@ -29,6 +32,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
+app.use('/api', routesApi);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -36,16 +42,32 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
+
+
 
 module.exports = app;
 
