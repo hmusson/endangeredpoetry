d181b6f907361d95c830bfbc87a0e28a4d7cbb93
Benji Friedman <benjifriedman@Benji.local>
2017-04-03 21:45:12 -0700

    poem routes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,13 +5,20 @@ var router = express.Router();
 var ctrlPoems = require('../controllers/poems');
 var ctrlComments = require('../controllers/comments');
 
-router.get('/poems', ctrlPoems.locationsListByDistance);
-router.post('/poems', ctrlPoems.locationsCreate);
-router.get('/poems/:poemid', ctrlPoems.locationsReadOne);
-router.put('/poems/:poemid', ctrlPoems.locationsUpdateOne);
-router.delete('/poems/:poemid', ctrlPoems.locationsDeleteOne);
+//router.get('/poems', ctrlPoems.locationsListByDistance);
+//originals, just for backup
+// router.post('/poems', ctrlPoems.locationsCreate);
+// router.get('/poems/:poemid', ctrlPoems.locationsReadOne);
+// router.put('/poems/:poemid', ctrlPoems.locationsUpdateOne);
+// router.delete('/poems/:poemid', ctrlPoems.locationsDeleteOne);
 
-// reviews
+router.post('/poems', ctrlPoems.poemsCreate); //create
+router.get('/poems/:poemid', ctrlPoems.poemsReadOne); //read
+router.put('/poems/:poemid', ctrlPoems.poemsUpdateOne); //update
+router.delete('/poems/:poemid', ctrlPoems.poemsDeleteOne); //mmmmove to the trash
+
+
+// reviews still have to be done
 router.post('/poems/:poemid/comments', ctrlComments.reviewsCreate);
 router.get('/poems/:poemid/comments/:commentid', ctrlComments.reviewsReadOne);
 router.put('/poems/:poemid/comments/:commentid', ctrlComments.reviewsUpdateOne);
