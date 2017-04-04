ac81e653054b906fb57e1da30ae878e47e5e7baf
Benji Friedman <benjifriedman@Benji.local>
2017-04-04 15:06:59 -0700

    updated two routes

--- app_api/controllers/comments.js
+++ app_api/controllers/comments.js
@@ -32,11 +32,11 @@ module.exports.commentsCreate = function(req, res) {
 
 var doAddComment = function(req, res, poem) {
   if (!poem) {
-    sendJSONresponse(res, 404, "locationid not found");
+    sendJSONresponse(res, 404, "poemid not found");
   } else {
     poem.comments.push({
       author: req.body.author,
-      rating: req.body.rating,
+      //rating: req.body.rating, //because we're not doing ratings
       reviewText: req.body.reviewText
     });
     poem.save(function(err, poem) {
@@ -52,90 +52,90 @@ var doAddComment = function(req, res, poem) {
   }
 };
 
-var updateAverageRating = function(locationid) {
-  console.log("Update rating average for", locationid);
-  Loc
-    .findById(locationid)
-    .select('reviews')
-    .exec(
-      function(err, location) {
-        if (!err) {
-          doSetAverageRating(location);
-        }
-      });
-};
+// var updateAverageRating = function(locationid) {
+//   console.log("Update rating average for", locationid);
+//   Loc
+//     .findById(locationid)
+//     .select('reviews')
+//     .exec(
+//       function(err, location) {
+//         if (!err) {
+//           doSetAverageRating(location);
+//         }
+//       });
+// };
 
-var doSetAverageRating = function(location) {
-  var i, reviewCount, ratingAverage, ratingTotal;
-  if (location.reviews && location.reviews.length > 0) {
-    reviewCount = location.reviews.length;
-    ratingTotal = 0;
-    for (i = 0; i < reviewCount; i++) {
-      ratingTotal = ratingTotal + location.reviews[i].rating;
-    }
-    ratingAverage = parseInt(ratingTotal / reviewCount, 10);
-    location.rating = ratingAverage;
-    location.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Average rating updated to", ratingAverage);
-      }
-    });
-  }
-};
+// var doSetAverageRating = function(location) {
+//   var i, reviewCount, ratingAverage, ratingTotal;
+//   if (location.reviews && location.reviews.length > 0) {
+//     reviewCount = location.reviews.length;
+//     ratingTotal = 0;
+//     for (i = 0; i < reviewCount; i++) {
+//       ratingTotal = ratingTotal + location.reviews[i].rating;
+//     }
+//     ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+//     location.rating = ratingAverage;
+//     location.save(function(err) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("Average rating updated to", ratingAverage);
+//       }
+//     });
+//   }
+// };
 
-module.exports.reviewsUpdateOne = function(req, res) {
-  if (!req.params.locationid || !req.params.reviewid) {
+module.exports.commentsUpdateOne = function(req, res) {
+  if (!req.params.poemid || !req.params.commentid) {
     sendJSONresponse(res, 404, {
-      "message": "Not found, locationid and reviewid are both required"
+      "message": "Not found, poemid and commentid are both required"
     });
     return;
   }
   Loc
-    .findById(req.params.locationid)
-    .select('reviews')
+    .findById(req.params.poemid)
+    .select('comments')
     .exec(
-      function(err, location) {
-        var thisReview;
-        if (!location) {
+      function(err, poem) {
+        var thisComment;
+        if (!poem) {
           sendJSONresponse(res, 404, {
-            "message": "locationid not found"
+            "message": "poemid not found"
           });
           return;
         } else if (err) {
           sendJSONresponse(res, 400, err);
           return;
         }
-        if (location.reviews && location.reviews.length > 0) {
-          thisReview = location.reviews.id(req.params.reviewid);
+        if (poem.comments && poem.comments.length > 0) {
+          thisComment = poem.comments.id(req.params.commentid);
           if (!thisReview) {
             sendJSONresponse(res, 404, {
-              "message": "reviewid not found"
+              "message": "commentid not found"
             });
           } else {
-            thisReview.author = req.body.author;
-            thisReview.rating = req.body.rating;
-            thisReview.reviewText = req.body.reviewText;
-            location.save(function(err, location) {
+            thisComment.author = req.body.author;
+            //thisReview.rating = req.body.rating; //because we're not doing ratings
+            thisComment.commentText = req.body.commentText;
+            poem.save(function(err, poem) {
               if (err) {
                 sendJSONresponse(res, 404, err);
               } else {
-                updateAverageRating(location._id);
-                sendJSONresponse(res, 200, thisReview);
+                updateAverageRating(poem._id);
+                sendJSONresponse(res, 200, thisComment);
               }
             });
           }
         } else {
           sendJSONresponse(res, 404, {
-            "message": "No review to update"
+            "message": "No comment to update"
           });
         }
       }
   );
 };
 
-module.exports.reviewsReadOne = function(req, res) {
+module.exports.commentsReadOne = function(req, res) {
   console.log("Getting single review");
   if (req.params && req.params.locationid && req.params.reviewid) {
     Loc
@@ -185,7 +185,7 @@ module.exports.reviewsReadOne = function(req, res) {
 };
 
 // app.delete('/api/locations/:locationid/reviews/:reviewid'
-module.exports.reviewsDeleteOne = function(req, res) {
+module.exports.commentsDeleteOne = function(req, res) {
   if (!req.params.locationid || !req.params.reviewid) {
     sendJSONresponse(res, 404, {
       "message": "Not found, locationid and reviewid are both required"
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var ctrlPoems = require('../controllers/poems');
 var ctrlComments = require('../controllers/comments');
 
+//old poem routes
 //router.get('/poems', ctrlPoems.locationsListByDistance);
 //originals, just for backup
 // router.post('/poems', ctrlPoems.locationsCreate);
@@ -17,11 +18,18 @@ router.get('/poems/:poemid', ctrlPoems.poemsReadOne); //read
 router.put('/poems/:poemid', ctrlPoems.poemsUpdateOne); //update
 router.delete('/poems/:poemid', ctrlPoems.poemsDeleteOne); //mmmmove to the trash
 
+//old comments/reviews routes
+// router.post('/poems/:poemid/comments', ctrlComments.reviewsCreate);
+// router.get('/poems/:poemid/comments/:commentid', ctrlComments.reviewsReadOne);
+// router.put('/poems/:poemid/comments/:commentid', ctrlComments.reviewsUpdateOne);
+// router.delete('/poems/:poemid/comments/:commentid', ctrlComments.reviewsDeleteOne);
 
-// reviews still have to be done
-router.post('/poems/:poemid/comments', ctrlComments.reviewsCreate);
-router.get('/poems/:poemid/comments/:commentid', ctrlComments.reviewsReadOne);
-router.put('/poems/:poemid/comments/:commentid', ctrlComments.reviewsUpdateOne);
-router.delete('/poems/:poemid/comments/:commentid', ctrlComments.reviewsDeleteOne);
+//new comments routes, I've done the commentsCreate and commentsUpdateOne
+//read and delete still have to be done
+router.post('/poems/:poemid/comments', ctrlComments.commentsCreate);
+router.get('/poems/:poemid/comments/:commentid', ctrlComments.commentsReadOne);
+router.put('/poems/:poemid/comments/:commentid', ctrlComments.commentsUpdateOne);
+router.delete('/poems/:poemid/comments/:commentid', ctrlComments.commentsDeleteOne);
+
 
 module.exports = router;
