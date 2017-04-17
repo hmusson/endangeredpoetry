46e6c8088fb226d689d337137b7a906950fe80d7
Eve Gottwald <Eve Gottwald>
2017-04-17 15:48:00 -0700

    cleaned some routes and controllers stuff up

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,28 +1,14 @@
-//change locations to poems
-
 var express = require('express');
 var router = express.Router();
 var ctrlPoems = require('../controllers/poems');
 var ctrlComments = require('../controllers/comments');
 
-//old poem routes
-//router.get('/poems', ctrlPoems.locationsListByDistance);
-//originals, just for backup
-// router.post('/poems', ctrlPoems.locationsCreate);
-// router.get('/poems/:poemid', ctrlPoems.locationsReadOne);
-// router.put('/poems/:poemid', ctrlPoems.locationsUpdateOne);
-// router.delete('/poems/:poemid', ctrlPoems.locationsDeleteOne);
 
 router.post('/poems', ctrlPoems.poemsCreate); //create
 router.get('/poems/:poemid', ctrlPoems.poemsReadOne); //read
 router.put('/poems/:poemid', ctrlPoems.poemsUpdateOne); //update
 router.delete('/poems/:poemid', ctrlPoems.poemsDeleteOne); //mmmmove to the trash
 
-//old comments/reviews routes
-// router.post('/poems/:poemid/comments', ctrlComments.reviewsCreate);
-// router.get('/poems/:poemid/comments/:commentid', ctrlComments.reviewsReadOne);
-// router.put('/poems/:poemid/comments/:commentid', ctrlComments.reviewsUpdateOne);
-// router.delete('/poems/:poemid/comments/:commentid', ctrlComments.reviewsDeleteOne);
 
 router.post('/poems/:poemid/comments', ctrlComments.commentsCreate);
 router.get('/poems/:poemid/comments/:commentid', ctrlComments.commentsReadOne);
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,31 +1,15 @@
 var express = require('express');
 var router = express.Router();
-//var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 var ctrlPoems = require('../controllers/poems');
 
 var ctrlTest = require('../controllers/testcontroller');
 
-/* Locations pages, old */
-// router.get('/', ctrlLocations.homelist);
-// router.get('/location', ctrlLocations.locationInfo);
-// router.get('/location/review/new', ctrlLocations.addReview);
-// router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
-
-//router.get('/poemList',ctrlLocations.poemList); 
-
-/* Poems pages, should be working, the error is somewhere in the controllers folder in controllers/poems */
+/* Poems pages */
 router.get('/', ctrlPoems.poemhome);
 router.get('/poem', ctrlPoems.poemInfo);
 router.get('/poem/comment/new', ctrlPoems.addComment);
 router.post('/poem/:poemid/comment/new', ctrlPoems.doAddComment);
-
-
-// //testing where the error is
-// router.get('/', ctrlTest.poemlist);
-// router.get('/poem', ctrlTest.poemInfo);
-// router.get('/poem/comment/new', ctrlTest.addComment);
-// router.post('/poem/:poemid/comment/new', ctrlTest.doAddComment);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
@@ -33,3 +17,9 @@ router.get('/poem2', ctrlOthers.poem2function);
 router.get('/poem3', ctrlOthers.poem3function);
 
 module.exports = router;
+
+// //testing where the error is
+// router.get('/', ctrlTest.poemlist);
+// router.get('/poem', ctrlTest.poemInfo);
+// router.get('/poem/comment/new', ctrlTest.addComment);
+// router.post('/poem/:poemid/comment/new', ctrlTest.doAddComment);
