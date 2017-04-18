15deee74f553b7685df8019ebc9b0ac1245fdbbf
Benji Friedman <benjifriedman@Benji.local>
2017-04-17 20:21:29 -0700

    took out some test controllers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,11 +2,3 @@
 module.exports.about = function(req, res){
   res.render('aboutpage', { title: 'About' });
 };
-
-module.exports.poem2function = function(req, res){
-	res.render('poem2', {title: 'anything'});
-};
-
-module.exports.poem3function = function(req, res){
-	res.render('singlepoemtest', {title: 'whatever'});
-};
