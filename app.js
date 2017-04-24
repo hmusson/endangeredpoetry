cf0aad7eca53b8a26d489a851ae4aba434233e6f
Benji Friedman <benjifriedman@gmail.com>
2017-04-23 19:24:19 -0700

    making it angular

--- app.js
+++ app.js
@@ -1,74 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-require('./app_api/models/db');
+// Node app 
 
-var routes = require('./app_client/routes/index');
+require('./app_api/models/db.js');  // using mongoose to connect to DB 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
 var routesApi = require('./app_api/routes/index');
 
-var app = express();
+app.set('port', process.env.PORT);
 
-// var index = require('./app_server/routes/index');
-// app.use('/', index);
+app.use(bodyParser.urlencoded({ extended: 'true' })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 
 
-// var users = require('./app_server/routes/users');
-// app.use('/users', users);
+app.use('/api', routesApi); 
 
-// var poemList = require('./app_server/routes/poemList');
-// app.use('/', poemList);
+app.use(express.static(path.join(__dirname, 'app_client')));
 
-// view engine setup
-app.set('views', path.join(__dirname,'app_client', 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/api', routesApi);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+var server = app.listen(app.get('port'), function() {
+       console.log('I am listening on port ' + server.address().port);
 });
 
 
 
-
-module.exports = app;
-
-// Wow!
