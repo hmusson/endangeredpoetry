b270a4458a2581ea52c2ce6be38f8e369ba435fe
Benji Friedman <benjifriedman@gmail.com>
2017-04-24 20:51:55 -0700

    angular changes

--- app_client/angular.app.js
+++ app_client/angular.app.js
@@ -5,16 +5,23 @@
   // $routeProvider allows to set up routes 
   function myConfig ($routeProvider) {
     $routeProvider
+      .when('/', {   template: '<p>This is the root page /  </p>',  })
+
+      .when('/about', { templateUrl: 'other/about.html'})
+
+
       .when('/writepoem', { 
-          templateUrl: 'views/writepoem.jade',
+          templateUrl: 'other/createhaiku.html',
           controller: 'writePoemController', controllerAs: 'writePoemCon' })
     
 
-    .when('/poem/:id', {
-    	  templateUrl: 'views/one_poem.jade',
+      .when('/poem/:id', {
+    	  templateUrl: 'other/onepoem.html',
         //TODO: make one_poem.jade file
-    	  controller: 'getOnePoemController', controllerAs: 'getOnePoemCon'
-    })
+    	  controller: 'getOnePoemController', controllerAs: 'getOnePoemCon'})
+
+      .otherwise( { redirectTo: '/'});  
+
 }
 
 })();
